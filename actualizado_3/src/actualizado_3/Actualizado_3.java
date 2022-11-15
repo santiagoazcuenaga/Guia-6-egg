9c813c0309ac582bd19317b0f46cdf32ea6c3a62
santiago de azcuenaga <santiagodeazcuenaga2001@gmail.com>
2022-11-15 13:22:25 -0300

    agregado nuevo ejercicio

---
+++ actualizado_3/src/actualizado_3/Actualizado_3.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package actualizado_3;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Actualizado_3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {    double radio = 0;
+circu cir = new circu(radio);
+cir.crearCircunferencia();
+
+        System.out.println(cir.perimetro());
+        System.out.println(cir.area());
+    }
+    
+}
---
+++ actualizado_3/src/actualizado_3/circu.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package actualizado_3;
+
+import java.util.Scanner;
+/**
+ *
+ * @author Usuario
+ */
+public class circu {
+      private double radio;
+
+    public circu(double radio) {
+        this.radio = radio;
+    }
+
+    public double getRadio() {
+        System.out.println("");
+        return radio;
+    }
+
+    public void setRadio(double radio) {
+        this.radio = radio;
+    }
+
+    public double crearCircunferencia() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("ingrese el radio");
+        radio = sc.nextDouble();
+        return this.radio=radio;
+    }    
+//public double area(double radio){
+//    double area= radio*radio*3.14;
+//    System.out.println("el area es: "+ (radio*3.14));
+//        return area;
+       
+  public double perimetro(){
+    
+        return this.radio * 2 * 3.14;
+               
+    }
+    
+    public double area(){
+        return this.radio *this.radio * 2 * 3.14;
+}
+}
